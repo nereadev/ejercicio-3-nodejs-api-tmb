7cf7e36ddef0431a20768a464171a990927fb318
Nerea García <nereagarcia.c@gmail.com>
2021-04-01 13:44:37 +0200

    levantar servidor y prueba primer get

--- index.js
+++ index.js
@@ -3,9 +3,22 @@ const debug = require("debug")("mi-app:principal");
 const express = require("express");
 const { program } = require("commander");
 const morgan = require("morgan");
+const { response } = require("express");
 
 program.option("-p, --puerto <puerto>", "Puerto para el servidor");
 program.parse(process.argv);
 const options = program.opts();
 const puerto = options.puerto || process.env.PUERTO || 5000;
-console.log(puerto);
+
+const app = express();
+
+const server = app.listen(puerto, () => {
+  debug(`Servidor escuchando en el puerto ${puerto}.`);
+});
+
+app.use(morgan("dev"));
+app.use(express.static("public"));
+app.get("/", (req, res, next) => {
+  debug("hola");
+  res.send("Acaba la prueba");
+});
