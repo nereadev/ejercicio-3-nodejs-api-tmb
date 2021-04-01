7a785bf25b25d8ddbe39ae3d85b6988684e591d5
Nerea García <nereagarcia.c@gmail.com>
2021-04-01 13:22:59 +0200

    opcion elegir puerto, por defecto 5000

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ const debug = require("debug")("mi-app:principal");
 const express = require("express");
 const { program } = require("commander");
 const morgan = require("morgan");
+
+program.option("-p, --puerto <puerto>", "Puerto para el servidor");
+program.parse(process.argv);
+const options = program.opts();
+const puerto = options.puerto || process.env.PUERTO || 5000;
+console.log(puerto);
