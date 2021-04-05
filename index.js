2fd565291d486b9d2e8ef09994528982fb31d69f
Nerea García <nereagarcia.c@gmail.com>
2021-04-05 20:49:38 +0200

    comprobar que se importa funcion conusltarLineas

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const { response } = require("express");
 const { consultarLineas } = require("./fetchApi");
 
 const url = `${process.env.API_URL}?app_id=${process.env.API_ID}&app_key=${process.env.API_KEY}`;
+consultarLineas(url);
 
 program.option("-p, --puerto <puerto>", "Puerto para el servidor");
 program.parse(process.argv);
