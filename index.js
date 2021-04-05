9b8ef1702a9a96f58d066e48ae268c9b74094c38
Nerea García <nereagarcia.c@gmail.com>
2021-04-05 11:34:27 +0200

    importar Lineas de la api

--- index.js
+++ index.js
@@ -5,7 +5,12 @@ const { program } = require("commander");
 const morgan = require("morgan");
 const { response } = require("express");
 
-program.option("-p, --puerto <puerto>", "Puerto para el servidor");
+const { consultarLineas } = require("./fetchApi");
+
+const url = `${process.env.API_URL}?app_id=${process.env.API_ID}&app_key=${process.env.API_KEY}`;
+consultarLineas(url);
+
+/* program.option("-p, --puerto <puerto>", "Puerto para el servidor");
 program.parse(process.argv);
 const options = program.opts();
 const puerto = options.puerto || process.env.PUERTO || 5000;
@@ -18,7 +23,8 @@ const server = app.listen(puerto, () => {
 
 app.use(morgan("dev"));
 app.use(express.static("public"));
-app.get("/", (req, res, next) => {
-  debug("hola");
+app.get("/metro/:lineas", (req, res, next) => {
+  const { lineas } = req.params;
   res.send("Acaba la prueba");
 });
+ */
