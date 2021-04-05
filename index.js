616f0b5b6bf3a1b52562592903d5642c47f9983f
Nerea García <nereagarcia.c@gmail.com>
2021-04-05 21:03:06 +0200

    mensajes error 404 y 500

--- index.js
+++ index.js
@@ -35,7 +35,10 @@ app.get("/metro/lineas/:linea?", (req, res, next) => {
 app.get("/", (req, res, next) => {
   res.redirect("/metro/lineas");
 });
+app.use((req, res, next) => {
+  res.status(404).json({ error: true, mensaje: "Recurso no encontrado" });
+});
 app.use((err, req, res, nett) => {
   debug(err);
-  res.status(500).send("Ha fallado algo.");
+  res.status(500).send({ error: true, mensaje: "Error general" });
 });
